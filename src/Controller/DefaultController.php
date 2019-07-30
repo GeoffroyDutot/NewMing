c92ba341f830c5fe184cdd8cf4551ec16a3a5b92
GeoffroyDutot <dutot.geoffroy@laposte.net>
2019-07-30 22:03:40 +0200

    save data in bdd

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,44 +2,73 @@
 
 namespace App\Controller;
 
+use App\Entity\Games;
+use App\Repository\GamesRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
+     * @var GamesRepository
+     */
+    private $gamesRepo;
+
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    public function __construct(GamesRepository $gamesRepo, ObjectManager $em)
+    {
+        $this->gamesRepo = $gamesRepo;
+        $this->em = $em;
+    }
+
+    /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(GamesRepository $gamesRepo)
     {
-       $games = [];
-
-       $client = \Symfony\Component\Panther\Client::createChromeClient();
-       $crawler = $client->request('GET', 'https://www.instant-gaming.com/fr/rechercher/?preorder=1&sort_by=avail_date_asc');
-       $fullPageHtml = $crawler->html();
-       $countitem = $crawler->filter('#ig-panel-center > div.search-wrapper > div.search > div')->count();
-       for($i=1; $i<$countitem+1; $i++){
-           $releasedate = $crawler->filter('#ig-panel-center > div.search-wrapper > div.search > div:nth-child('.$i.') > div.name')->text();
-           $title = $crawler->filter('#ig-panel-center > div.search-wrapper > div.search > div:nth-child('.$i.') > a > img')->attr('alt');
-           $url = $crawler->filter('#ig-panel-center > div.search-wrapper > div.search > div:nth-child('.$i.') > a > img')->attr('src');
-           $url = str_replace('157x218', '271x377', $url);
-           $price = $crawler->filter('#ig-panel-center > div.search-wrapper > div.search > div:nth-child('.$i.') > a > div > div.price')->text();
-
-           $game = [
-               "title" => $title,
-               "releasedate" => $releasedate,
-               "url" => $url,
-               "price" => $price
-           ];
-
-           $games[] = $game;
-       }
-
-        $client->quit();
-
         return $this->render('default/index.html.twig', [
-            "games" => $games
+            "games" => $gamesRepo->findAll()
             ]
         );
+    }
+
+    /**
+     * @Route("/scrapping", name="scrapping")
+     */
+    public function scrapping()
+    {
+        //remove all the data in the games table
+        $connection = $this->em->getConnection();
+        $platform   = $connection->getDatabasePlatform();
+        $connection->executeUpdate($platform->getTruncateTableSQL('games', true));
+
+        //scrape the data from IG
+        $client = \Symfony\Component\Panther\Client::createChromeClient();
+        $crawler = $client->request('GET', 'https://www.instant-gaming.com/fr/rechercher/?preorder=1&sort_by=avail_date_asc');
+        $fullPageHtml = $crawler->html();
+        $countitem = $crawler->filter('#ig-panel-center > div.search-wrapper > div.search > div')->count();
+        for($i=1; $i<$countitem+1; $i++){
+            $releasedate = $crawler->filter('#ig-panel-center > div.search-wrapper > div.search > div:nth-child('.$i.') > div.name')->text();
+            $title = $crawler->filter('#ig-panel-center > div.search-wrapper > div.search > div:nth-child('.$i.') > a > img')->attr('alt');
+            $url = $crawler->filter('#ig-panel-center > div.search-wrapper > div.search > div:nth-child('.$i.') > a > img')->attr('src');
+            $url = str_replace('157x218', '271x377', $url);
+            $price = $crawler->filter('#ig-panel-center > div.search-wrapper > div.search > div:nth-child('.$i.') > a > div > div.price')->text();
+
+            $scrapping = new Games();
+            $scrapping->setTitle($title);
+            $scrapping->setPrice($price);
+            $scrapping->setReleasedate($releasedate);
+            $scrapping->setUrl($url);
+            $this->em->persist($scrapping);
+            $this->em->flush();
+        }
+        $client->quit();
+
+        return $this->redirectToRoute('home');
     }
 }
---
+++ src/Entity/Games.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GamesRepository")
+ */
+class Games
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $releasedate;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $url;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getReleasedate(): ?string
+    {
+        return $this->releasedate;
+    }
+
+    public function setReleasedate(?string $releasedate): self
+    {
+        $this->releasedate = $releasedate;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}
